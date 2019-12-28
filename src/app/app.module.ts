743900850d174e949fe363ab8043a57137858f94
Rohit Pardeshi <rohitmpardeshi0411@gmail.com>
2019-12-28 10:47:24 +0530

    T-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BlogDetailComponent } from './components/blog-detail/blog-detail.component';
 import { BlogListComponent } from './components/blog-list/blog-list.component';
 import { BlogComponent } from './components/blog/blog.component';
-import {MatIconModule, MatIcon} from '@angular/material/icon';
+import { MatIconModule, MatIcon } from '@angular/material/icon';
 
 import { environment } from '../environments/environment'
 import { AngularFireModule } from 'angularfire2'
-import {AngularFirestoreModule } from 'angularfire2/firestore'
+import { AngularFirestoreModule } from 'angularfire2/firestore'
 import { BlogService } from './services/blog.service';
 import { NewBlogComponent } from './components/new-blog/new-blog.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
@@ -19,8 +19,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { MatProgressBarModule } from '@angular/material/progress-bar'
 import { AngularFireStorageModule } from 'angularfire2/storage';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import { EscapeHtmlPipe } from './pipes/keep-html.pipe';
 import { AdminComponent } from './components/admin/admin.component';
@@ -45,9 +45,9 @@ import { AdminLoginComponent } from './components/admin-login/admin-login.compon
 import { AdminEventsComponent } from './components/admin-events/admin-events.component';
 
 
-import {MatButtonModule, MatInputModule, MatSliderModule, MatDialogModule } from '@angular/material';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatButtonModule, MatInputModule, MatSliderModule, MatDialogModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material';
 import { AdminViewComponent } from './components/admin-view/admin-view.component';
 import { AdminVdetailsComponent } from './components/admin-vdetails/admin-vdetails.component';
@@ -56,6 +56,7 @@ import { TableComponent } from './components/table/table.component';
 
 import { RecaptchaModule, RecaptchaFormsModule } from 'ng-recaptcha';
 import { AdminFeedbackComponent } from './components/admin-feedback/admin-feedback.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -90,7 +91,8 @@ import { AdminFeedbackComponent } from './components/admin-feedback/admin-feedba
     AdminVdetailsComponent,
     VerifyEmailComponent,
     TableComponent,
-    AdminFeedbackComponent
+    AdminFeedbackComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -110,7 +112,7 @@ import { AdminFeedbackComponent } from './components/admin-feedback/admin-feedba
     MatProgressSpinnerModule,
     CKEditorModule,
     AngularFireAuthModule,
-    ReactiveFormsModule, FormsModule,BrowserAnimationsModule,
+    ReactiveFormsModule, FormsModule, BrowserAnimationsModule,
     MatButtonModule, MatInputModule, MatSliderModule, MatDialogModule,
     MatFormFieldModule,
     MatDatepickerModule,
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ElementRef } from "@angular/core";
 
-import {EventService } from '../../services/event.service';
-import {Params, Router,ActivatedRoute} from '@angular/router';
+import { EventService } from '../../services/event.service';
+import { Params, Router, ActivatedRoute } from '@angular/router';
 import $ from "jquery";
 
 @Component({
@@ -11,12 +11,12 @@ import $ from "jquery";
   styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-  
+
   items: any = [];
-  
-  constructor(private elementRef: ElementRef ,  public eventService: EventService,
+
+  constructor(private elementRef: ElementRef, public eventService: EventService,
     private router: Router,
-    private route: ActivatedRoute) {}
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     var s = document.createElement("script");
@@ -30,54 +30,54 @@ export class HomeComponent implements OnInit {
     this.elementRef.nativeElement.appendChild(s1);
 
 
-    $(window).scroll(function() {
-      $(".button").css("opacity", 0 + $(window).scrollTop() / 500);
-    });
-    $(window).scroll(function() {
-      $(".home-footer").css("opacity", 1 - $(window).scrollTop() / 300);
-    });
-    $(document).ready(function() {
-      $("#box1").click(function() {
+    // $(window).scroll(function () {
+    //   $(".button,.button1").css("opacity", 0 + $(window).scrollTop() / 500);
+    // });
+    // $(window).scroll(function () {
+    //   $(".home-footer").css("opacity", 1 - $(window).scrollTop() / 300);
+    // });
+    $(document).ready(function () {
+      $("#box1").click(function () {
         $("#e0,#e2,#e3,#e4").css({ display: "none" });
         $("#e1, #close").fadeIn(2000);
       });
-      $("#box2").click(function() {
+      $("#box2").click(function () {
         $("#e0,#e1,#e3,#e4").css({ display: "none" });
         $("#e2, #close").fadeIn(2000);
       });
-      $("#box3").click(function() {
+      $("#box3").click(function () {
         $("#e0,#e1,#e2,#e4").css({ display: "none" });
         $("#e3, #close").fadeIn(2000);
       });
-      $("#box4").click(function() {
+      $("#box4").click(function () {
         $("#e0,#e1,#e2,#e3").css({ display: "none" });
         $("#e4, #close").fadeIn(2000);
       });
-      $("#close").click(function() {
+      $("#close").click(function () {
         $("#e1,#e2,#e3,#e4,#close").css({ display: "none" });
         $("#e0").fadeIn(2000);
       });
     });
-    
+
     setTimeout(() => {
-      this.getData() ;
-     
+      this.getData();
+
     }, 2000);
-   
+
 
   }
 
   getData() {
     this.eventService.getUsers()
-    .subscribe(result => {
-      console.log(result);
-      result.forEach((item) => {
-        console.log(item);
-        this.items.push(item);
-      }) 
-      
-    
-    })
+      .subscribe(result => {
+        console.log(result);
+        result.forEach((item) => {
+          console.log(item);
+          this.items.push(item);
+        })
+
+
+      })
   }
 
 
